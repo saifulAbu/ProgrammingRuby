592e3c4a11922e5a4d9eea489e6d84ce41e38dec
siddartha saif <saifulabu25@gmail.com>
2018-08-17 07:54:06 -0500

    Added notes

---
+++ 4/array.rb
@@ -0,0 +1,42 @@
+a = [3.1419, "pie", 99]
+a.class
+a.length
+a[0]
+
+a = [ 1, 2, 3, 4, 5 ]
+# access array using [] operator
+# last index = -1
+a[-1]
+a[-99] #nil
+
+#[start_index, element_count]
+a[1, 3]
+
+#[start_index..end_index]
+
+# stack
+stack = []
+stack.push "red"
+stack.push "green"
+stack.push "blue"
+stack
+
+stack.pop
+stack.pop
+stack.pop
+stack
+
+# queue
+queue = []
+queue.push "red"
+queue.push "green"
+
+queue.shift
+queue.shift
+
+# non destructive access
+array = [1, 2, 4, 5, 6]
+array.first(3)
+array.last(2)
+
+
