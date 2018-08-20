b14012c650d3f328d55fcfc8b7c40036edebcb68
siddartha saif <saifulabu25@gmail.com>
2018-08-19 22:32:10 -0500

    Hash

---
+++ 4/hashes.rb
@@ -0,0 +1,12 @@
+h = { 'dog' => 'canine', 'cat' => 'feline', 'donkey' => 'asinine' }
+
+h.length
+h['dog'] #retrive
+h['cow'] = 'bovine' #assign 
+
+# keys can be symbols
+h = {:dog => 'canine', :cat => 99, :donkey => "asinine"}
+
+#syntactic sugar
+h = {dog: 'canine', cat: 'feline', donkey: 'asinine'}
+
