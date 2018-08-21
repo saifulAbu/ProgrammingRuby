1ac2f9589e4c5f586a2289506c33b5973789d245
siddartha saif <saifulabu25@gmail.com>
2018-08-21 07:33:33 -0500

    end test

--- 4/test_count_frequency.rb
+++ 4/test_count_frequency.rb
@@ -9,5 +9,17 @@ class TestCountFrequency < Test::Unit::TestCase
 	def test_single_word
 		assert_equal({"cat" => 1}, count_frequency(["cat"]))
 	end
+
+	def test_two_different_words
+		assert_equal({"cat" => 1, "sat" => 1}, count_frequency(["cat", "sat"]))
+	end
+
+	def test_two_words_adjacent_repeat
+		assert_equal({"cat" => 2, "sat" => 1}, count_frequency(["cat", "cat", "sat"]))
+	end
+
+	def test_two_words_non_adjacent_repeat
+		assert_equal({"cat" => 2, "sat" => 1}, count_frequency(["cat", "sat", "cat"]))
+	end
 end
 
