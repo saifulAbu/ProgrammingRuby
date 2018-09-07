9fc04b7979023535df7b5083100d75f827b1c9c1
siddartha saif <saifulabu25@gmail.com>
2018-09-07 08:10:39 -0500

    add range

--- 6/ranges.rb
+++ 6/ranges.rb
@@ -13,3 +13,52 @@ enum = ('bar'..'bat').to_enum
 p enum.next
 p enum.next
 
+# example of methods
+digits = 0..9
+digits.include?(5) # true
+digits.max   # 9
+digits.reject {|i| i < 5} # [5, 6,7,8,9]
+digits.inject(:+) # 45
+
+# user specified range
+class PowerOfTwo
+	attr_reader :value
+	def initialize(value)
+		@value = value
+	end
+	def <=>(other)
+		@value <=> other.value
+	end
+	def succ
+		PowerOfTwo.new(@value + @value)
+	end
+	def to_s
+		@value.to_s
+	end
+end
+
+p1 = PowerOfTwo.new(4)
+p2 = PowerOfTwo.new(32)
+
+puts (p1..p2).to_a
+
+# ranges as intervals
+(1..10) === 5 #true
+(1..10) === 15 #false
+(1..10) === 3.14159 #true
+
+# case statement
+car_age = gets.to_f
+case car_age
+when 0...1
+	puts "Brand new"
+when 1...3
+	puts "Nice and new"
+when 3...10
+	puts "Reliable but slightly dinged"
+when 10...30
+	puts "clunker"
+else 
+	puts "vintage gem"
+end
+
