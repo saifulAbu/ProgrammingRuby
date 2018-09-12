11a4d8e8a73673a88b68aef378aa16a84a902b9f
siddartha saif <saifulabu25@gmail.com>
2018-09-12 06:55:11 -0500

    Adding anchor explanation for \b and \B

--- 7/anchor.rb
+++ 7/anchor.rb
@@ -5,6 +5,8 @@ p show_regexp(str, /^the/) #start of line
 p show_regexp(str, /is$/)  #end of line
 p show_regexp(str, /\Athis/) #start of line
 p show_regexp(str, /\Athe/) 
+# \b matches beginning or end of a string
+# \B matches not beginning or end of a string
 p show_regexp(str, /\bis/) # word boundaries
 p show_regexp(str, /\Bis/) # non-word boundaries
 
