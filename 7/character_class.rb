424045276bc79cbca3fc9dd2f6062aab3cb3671b
siddartha saif <saifulabu25@gmail.com>
2018-09-14 21:20:24 -0500

    more char class

--- 7/character_class.rb
+++ 7/character_class.rb
@@ -3,3 +3,9 @@ require './show_match.rb'
 p show_regexp('Price $12.', /[aeiou]/)
 p show_regexp('Price $12.', /[\s]/)
 p show_regexp('Price $12.', /[$.]/)
+
+a = 'see [The PickAxe-page 123]'
+p show_regexp(a, /[A-F]/)
+p show_regexp(a, /[A-Fa-f]/)
+p show_regexp(a, /[0-9]/)
+p show_regexp(a, /[0-9][0-9]/)
