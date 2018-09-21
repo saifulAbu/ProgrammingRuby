e3371f702929f436a1e84c99aedafe46a92b2754
siddartha saif <saifulabu25@gmail.com>
2018-09-21 07:07:03 -0500

    backslash with named groups

--- 7/back_slash_seq.rb
+++ 7/back_slash_seq.rb
@@ -1,3 +1,7 @@
+# using matched group to match
 puts "fred:smith".sub(/(\w+):(\w+)/, '\2, \1')
 puts "nercpyitno".gsub(/(.)(.)/, '\2\1')
 
+# named groups
+puts "fred:smith".sub(/(?<first>\w+):(?<last>\w+)/, '\k<last>, \k<first>')
+puts "nercpyitno".gsub(/(?<c1>.)(?<c2>.)/, '\k<c2>\k<c1>')
