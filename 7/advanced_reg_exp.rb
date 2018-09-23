b8a0f6ac8bb575034a5cc8faed008ec8aef24020
siddartha saif <saifulabu25@gmail.com>
2018-09-23 07:31:38 -0500

    advanced regex

---
+++ 7/advanced_reg_exp.rb
@@ -0,0 +1,5 @@
+# you usually written on (? and ).
+# (?#comment) -> inserts a comment into the pattern
+# (?:reg_ex) reg_ex creates group without generating backreferences
+
+date = "12/25/2010"
--- 7/back_slash_seq.rb
+++ 7/back_slash_seq.rb
@@ -7,4 +7,22 @@ puts "fred:smith".sub(/(?<first>\w+):(?<last>\w+)/, '\k<last>, \k<first>')
 puts "nercpyitno".gsub(/(?<c1>.)(?<c2>.)/, '\k<c2>\k<c1>')
 
 #string subs
+puts "does not work for string double evaluation"
 str = 'a\b\c'
+puts str.gsub(/\\/, '\\\\')
+
+puts "the following works"
+str = 'a\b\c'
+puts str.gsub(/\\/, '\\\\\\\\')
+
+# \& -> matched string
+puts "\& works too"
+str = 'a\b\c'
+puts str.gsub(/\\/, '\&\&')
+
+# string in a block gets evaluated only once
+puts "gsub using block"
+str = 'a\b\c'
+puts str.gsub(/\\/) { '\\\\' }
+
+
