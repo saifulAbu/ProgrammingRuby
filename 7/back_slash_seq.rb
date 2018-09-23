b677c9339d14825873176116c0fbce58244705c0
siddartha saif <saifulabu25@gmail.com>
2018-09-22 22:53:54 -0500

    more..

--- 7/back_slash_seq.rb
+++ 7/back_slash_seq.rb
@@ -5,3 +5,6 @@ puts "nercpyitno".gsub(/(.)(.)/, '\2\1')
 # named groups
 puts "fred:smith".sub(/(?<first>\w+):(?<last>\w+)/, '\k<last>, \k<first>')
 puts "nercpyitno".gsub(/(?<c1>.)(?<c2>.)/, '\k<c2>\k<c1>')
+
+#string subs
+str = 'a\b\c'
