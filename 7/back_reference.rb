8a6336e33ed1b7596a4bb79156b31c12e796b37b
siddartha saif <saifulabu25@gmail.com>
2018-09-27 20:45:48 -0500

    named back reference

--- 7/back_reference.rb
+++ 7/back_reference.rb
@@ -5,3 +5,36 @@ differ = "12:45-13:15"
 pat = /(...)\1/ 
 p pat =~ 'abcabc'
 p pat =~ 'abcaba'
+
+# example: match same hour part
+pat = /(\d\d):\d\d-\1:\d\d/
+
+same = "12:15-12:45"
+differ = "12:45-13:15"
+
+p pat =~ same # => 0
+p pat =~ differ # => nil
+
+
+# use name back reference
+pat = /(?<hour>\d\d):\d\d-\k<hour>:\d\d/
+
+p pat =~ same
+p pat =~ differ
+
+# negative backreference number count backward
+pat = /(.)(.)\k<-1>\k<-2>/
+
+p pat =~ "abab" # no match
+p pat =~ "abba" # match
+
+# named sub-pattern
+pat = /(?<color>red|green|blue) \w+ \g<color> \w+/
+
+p pat =~ "red sun blue moon" # match
+p pat =~ "red sun white moon" # no-match
+
+
+
+
+
