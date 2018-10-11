c9fb404a4a04148a2320638e885b5176ec11f411
siddartha saif <saifulabu25@gmail.com>
2018-10-10 22:02:28 -0500

    method

---
+++ 8/variable-len-arg.rb
@@ -0,0 +1,25 @@
+class Parent
+	def do_something(*param)
+		puts "#{param.inspect}"
+	end
+end
+
+class Child < Parent
+	def do_something(*not_used)
+		super
+		# processing
+	end
+end
+
+def split_apart(first, *splat, last)
+	puts "First: #{first.inspect}, splat: #{splat.inspect}, last: #{last.inspect}"
+end
+
+a = Child.new
+a.do_something(1, 2, 3, 4, 5) 
+
+split_apart(1, 2)
+split_apart(1, 2, 3)
+split_apart(1, 2, 3, 4)
+
+
