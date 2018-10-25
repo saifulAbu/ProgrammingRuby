ff421cd35f3b7da3c3b1556b74b42f2b4d76a6c9
siddartha saif <saifulabu25@gmail.com>
2018-10-25 07:37:51 -0500

    more case

--- 9/case_expressions.rb
+++ 9/case_expressions.rb
@@ -1,3 +1,4 @@
+# case basic
 case 
 when song.name = "misty"
 	puts "not again"
@@ -9,4 +10,45 @@ else
 	song.play
 end
 
+# case 
+case command
+when "debug"
+	dump_debug_info
+	dump_symbols
+when /p|s+(\w+)/
+	dump_variable($1)
+when "quit", "exit"
+	exit
+else 
+	print "illegal command: #{commmand}"
+end
+
+# compact
+kind = 	case year
+				when 1850..1889 then "Blues"
+				when 1890..1909 then "Ragtime"
+				when 1910..1929 then "New Orleans Jazz"
+				when 1930..1939 then "Swing"
+				else 								 "jazz"
+				end
+
+# in case expressions var === val is applied
+case line
+when /title=(.*)/
+	puts "title is #$1"
+when /track=(.*)/
+	puts "Track is #$1"
+end
+
+# 
+case shape
+when Square, Rectangle
+	# ...
+when Circle
+	# ...
+when Trianble
+	# ...
+else
+	# ...
+end
 
