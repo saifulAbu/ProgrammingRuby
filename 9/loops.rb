62da5abc7ade64cb133064b14149f50ea7b9a877
siddartha saif <saifulabu25@gmail.com>
2018-10-30 07:34:02 -0500

    more loops

--- 9/loops.rb
+++ 9/loops.rb
@@ -13,4 +13,53 @@ a = 1
 a *= 2 while a < 100
 a -= 10 until a < 100
 
+# times
+3.times do
+	print "Ho! "
+end
+
+# upto
+0.upto(9) do |x|
+	print x, " "
+end
+
+# step
+0.step(12, 3) {|x| print x, " "}
+
+# loop
+loop do
+  # block...
+end
+
+# for..in
+for song in playlist
+	song.play
+end
+
+# is a syntactacit sugar for the following
+playlist.each do |song|
+	song.play
+end
+
 # 
+for i in [1, 2, 3, 4]
+	print i
+end
+
+for i in 1..3
+	print i
+end
+
+# class
+class Periods
+	def each
+		yield "classical"
+		yield "jazz"
+		yield "rock"
+	end
+end
+
+periods = Periods.new
+for genre in periods
+	print genre
+end
