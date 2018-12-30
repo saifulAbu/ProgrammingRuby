e1ce38853ec6be6883473ba0d0fcab2b2266251a
siddartha saif <saifulabu25@gmail.com>
2018-12-29 21:08:47 -0600

    adding test method to understand method definition creation

---
+++ 24/test-metaprogramming.rb
@@ -0,0 +1,18 @@
+class Test
+	def self.abc
+		puts "class method getting called"
+		def def
+			puts "hello there!"
+			def ghi
+				puts "method ghi"
+			end
+		end
+	end
+	
+	abc
+end
+
+a = Test.new
+b = a.def
+a.ghi
+
