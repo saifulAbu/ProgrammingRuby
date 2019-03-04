ddcb851eccbf2e80aefdd7347d00c8478e3ce447
siddartha saif <saifulabu25@gmail.com>
2019-03-04 05:55:39 -0600

    adding processes

---
+++ 12/child-process.rb
@@ -0,0 +1,9 @@
+pipe = IO.popen("-", "w+")
+if pipe
+  pipe.puts "Get a job"
+	  STDERR.puts "Child says '#{pipe.gets.chomp}'"
+		else
+		  STDERR.puts "Dad says '#{gets.chomp}'"
+			  puts "Ok"
+				end
+
