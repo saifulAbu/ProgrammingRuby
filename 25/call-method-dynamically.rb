ae985ec8a52d153b18f502c84e0cf86621bb3c87
siddartha saif <saifulabu25@gmail.com>
2019-04-09 07:02:56 -0500

    add method dynamics

--- 25/call-method-dynamically.rb
+++ 25/call-method-dynamically.rb
@@ -16,4 +16,35 @@ end
 method_object = method(:double)
 puts [ 1, 3, 5, 7 ].map(&method_object)
 
+unbound_length = String.instance_method(:length)
 
+class String
+	def length
+		99
+	end
+end
+
+# bind instance method
+str = "cat"
+puts str.length
+bound_length = unbound_length.bind(str)
+bound_length.call
+
+# eval
+trane = %q{ "John Coltrane".length }
+miles = %q{ "Miles Davis".sub(/iles/, '.')  }
+
+eval trane
+eval miles
+
+# binding a state of an object
+def get_a_binding
+	val = 123
+	binding
+end
+
+val = "cat"
+
+the_binding = get_a_binding
+eval("val", the_binding)
+eval("val")
